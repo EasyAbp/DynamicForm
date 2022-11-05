f431e0f497cef0e9952891b86d91b8c375259cc4
gdlcf88 <gdlcf88@outlook.com>
2022-11-05 14:24:39 +0800

    Add `DynamicFormOptions`

---
+++ src/EasyAbp.DynamicForm.Domain/EasyAbp/DynamicForm/Options/DynamicFormOptions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace EasyAbp.DynamicForm.Options;
+
+public class DynamicFormOptions
+{
+    protected Dictionary<string, FormDefinition> FormDefinitions { get; } = new();
+
+    public FormDefinition GetFormDefinition(string formDefinitionName)
+    {
+        return FormDefinitions[formDefinitionName];
+    }
+
+    public void AddOrUpdateFormDefinition(FormDefinition formDefinition)
+    {
+        FormDefinitions[formDefinition.Name] = formDefinition;
+    }
+}
