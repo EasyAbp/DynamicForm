ea60a8289fe8f9de6fb24f6509d6fe67737645a6
gdlcf88 <gdlcf88@outlook.com>
2022-11-10 22:11:07 +0800

    Add `TextBoxFormItemConfigurations.TextBoxRows`

--- src/EasyAbp.DynamicForm.Domain.Core/EasyAbp/DynamicForm/FormItemTypes/TextBox/TextBoxFormItemConfigurations.cs
+++ src/EasyAbp.DynamicForm.Domain.Core/EasyAbp/DynamicForm/FormItemTypes/TextBox/TextBoxFormItemConfigurations.cs
@@ -4,6 +4,8 @@ namespace EasyAbp.DynamicForm.FormItemTypes.TextBox;
 
 public class TextBoxFormItemConfigurations
 {
+    public int TextBoxRows { get; set; } = 1;
+
     public int? MaxLength { get; set; }
 
     public int? MinLength { get; set; }
