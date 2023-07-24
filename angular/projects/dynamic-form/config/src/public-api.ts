71be79e2c9d504e8a0ea9b353b705016ae041af4
gdlcf88 <gdlcf88@outlook.com>
2023-07-24 15:05:47 +0800

    Upgrade to ABP 7.3.0

--- angular/projects/dynamic-form/config/src/public-api.ts
+++
@@ -1,3 +0,0 @@
-export * from './enums';
-export * from './dynamic-form-config.module';
-export * from './providers';
--- angular/projects/dynamic-form/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of dynamic-form
- */
-
-export * from './lib/components/dynamic-form.component';
-export * from './lib/services/dynamic-form.service';
-export * from './lib/dynamic-form.module';
